d7a33d16b49e75c9a5bb766d782dcab55afa9b9f
Thomas Boerger <thomas@webhippie.de>
2020-02-24 11:20:17 +0100

    Add more tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,3 +16,7 @@ def test_running_and_enabled(host):
     svc = host.service("telegraf")
     assert svc.is_running
     assert svc.is_enabled
+
+
+def test_server_socket(host):
+    assert host.socket("tcp://127.0.0.1:9273").is_listening
